f07a57610f6afaa6f9857d352c48f9124f679329
Camilo Mujica <printcamilo@gmail.com>
2024-03-21 22:49:41 -0300

    subida de carpetas

---
+++ fonasa-app/fonasa-frontend/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="https://s3.amazonaws.com/sodlabx-filemanager-dev/uploads/5D6dh5HXGeFQbhEWi-Sinttulo.jpg" />
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
